3c36f768fae652f76e6a14ec38c916439488c190
Kevin Picoli Silveira <47861834+Kevin6476@users.noreply.github.com>
2022-05-22 18:28:16 -0300

    OO básico, isolando atributos (infos) e métodos (ações) sobre uma classe representativa de conta bancária. herança e polimorfismo não abordado

--- Account.py
+++ Account.py
@@ -1,9 +1,54 @@
 class Account:
 
-    def __init__(self, number, holder, sale, limit):
-        print("Inicializando classe... {}".format(self))
-        self.number = number
-        self.holder = holder
-        self.sale = sale
-        self.limit = limit
+    def __init__(self, account_number, titular_name, bank_balance, credit_limit):
+        print(self)
+        self.__account_number = account_number
+        self.__titular_name = titular_name
+        self.__bank_balance = bank_balance
+        self.__credit_limit = credit_limit
+
+    def extract(self):
+        print("The account of '{0}' has a value of R$ {1:.2f}".format(self.__titular_name, self.__bank_balance))
+
+    def __you_can_withdraw(self, amount_to_be_withdrawn):
+        available_value = self.bank_balance + self.credit_limit
+        return amount_to_be_withdrawn <= available_value
+
+    def deposit(self, value):
+        self.__bank_balance += value
+
+    def to_withdraw(self, value):
+        if self.__you_can_withdraw(value):
+            self.__bank_balance -= value
+        else:
+            print("Operation not performed! Insufficient funds")
+
+    def transfer(self, value, destination_account):
+        if self.__you_can_withdraw(value):
+            self.to_withdraw(value)
+            destination_account.deposit(value)
+            print("Transfer successful!")
+        else:
+            print("Operation not performed! Insufficient funds")
+
+    @property
+    def titular_name(self):
+        return self.__titular_name
+
+    @property
+    def bank_balance(self):
+        return self.__bank_balance
+
+    @property
+    def credit_limit(self):
+        return self.__credit_limit
+
+    @credit_limit.setter
+    def credit_limit(self, credit_limit):
+        self.__credit_limit = credit_limit
+
+    @staticmethod
+    def bank_code():
+        return "001"
+
 
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from Account import Account
 
-count = Account()
+account1 = Account(account_number=1, titular_name="Kevin", bank_balance=1500, credit_limit=500.99)
+account2 = Account(account_number=2, titular_name="João", bank_balance=3000, credit_limit=785.33)
+account3 = Account(account_number=3, titular_name="André", bank_balance=845.5, credit_limit=459.14)
 
+#printing balance and name
+account1.extract()
+account2.extract()
+account3.extract()
 
-print("Executando", count)
+transfer_value = 250.11
+print("{0} will transfer R$ {2:.2f} to {1}".format(account1.titular_name, account3.titular_name, transfer_value))
+account1.transfer(250.11, account3)
+
+#printing balance and name
+account1.extract()
+account2.extract()
+account3.extract()
